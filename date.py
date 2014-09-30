efe0738f1b26ad34967b2ba650c47e7995ad5f3e
44746 <44746@longroad.ac.uk>
2014-09-30 13:00:29 +0100

    date

---
+++ date.py
@@ -0,0 +1,19 @@
+#George West
+#30-09-2014
+#date
+
+day = int(input("Please input the number of the day: e.g 27"))
+month = int(input("Please input the number of the month: e.g 11"))
+year = int(input("Please input the number of the day: e.g 27"))
+
+if 0<day<32 and 0<month<13:
+    if day == 1:
+        print("1st")
+    elif day == 21:
+        print("21st")
+    elif day == 2:
+        print("2nd")
+    elif day == 22:
+        print("22nd")
+    
+    
