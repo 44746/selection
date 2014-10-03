1eb73828730648ed21965f27a0db630f9f59de84
44746 <44746@longroad.ac.uk>
2014-10-03 13:33:15 +0100

    grade calculator

---
+++ grade calculator.py
@@ -0,0 +1,36 @@
+#George West
+#3-10-14
+#Grade calculator
+
+#pseudocode
+
+#Grade calculator
+#Get grade
+#IF grade > 80 THEN
+#   OUTPUT "A"
+#ELIF grade > 70 THEN
+#   OUTPUT "B"
+#ELIF grade > 60 THEN
+#   OUTPUT "C"
+#ELIF grade > 50 THEN
+#   OUTPUT "D"
+#ELIF grade > 40 THEN
+#   OUTPUT "E"
+#ELSE
+#   OUTPUT"U"
+# END IF
+
+score= int(input("Please enter your score: "))
+print("You got was a")
+if score > 80: 
+   print("A")
+elif score > 70:
+    print("B")
+elif score > 60:
+    print("C")
+elif score > 50:
+   print("D")
+elif score > 40:
+   print("E")
+else:
+   print("U")
