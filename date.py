c7160929731006a2caeee7b854efe84c5ed68582
44746 <44746@longroad.ac.uk>
2014-10-03 14:32:25 +0100

    DATE

--- date.py
+++ date.py
@@ -6,6 +6,43 @@
 #GET day
 #GET month
 #GET year
+#IF year == 12 THEN
+#   month = "December"
+#IF year == 11 THEN
+#   month = "November"
+#IF year == 10 THEN
+#   month = "October"
+#IF year == 9 THEN
+#   month = "September"
+#IF year == 8 THEN
+#   month = "August"
+#IF year == 7 THEN
+#   month = "july"
+#IF year == 6 THEN
+#   month = "June"
+#IF year == 5 THEN
+#   month = "May"
+#IF year == 4 THEN
+#   month = "April"
+#IF year == 3 THEN
+#   month = "March"
+#IF year == 2 THEN
+#   month = "February"
+#IF year == 1 THEN
+#   month = "January"
+#IF day == 1 THEN
+#        OUTPUT "1st {0} {1}" +month, year
+#ELIF day == 21 THEN
+#        OUTPUT "21st {0} {1}" +month, year
+#ELIF day == 31 THEN
+#        OUTPUT "31st {0} {1}" +month, year
+#ELIF day == 2 THEN
+#        OUTPUT "2ND {0} {1}" +month, year
+#ELIF day == 22 THEN
+#        OUTPUT "22ND {0} {1}" +month, year
+#ELIF day == 3 THEN
+#        OUTPUT "3RD {0} {1}" +month, year
+# END IF
 day = int(input("Please input the number of the day: e.g 27  "))
 month = int(input("Please input the number of the month: e.g 11  "))
 year = int(input("Please input the number of the year: e.g 2013  "))
